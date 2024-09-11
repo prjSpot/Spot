e0a5541952eb49fb2011533d5c3fea9331810603
vloeiolzlr <yoonseed1995@naver.com>
2024-09-11 13:23:16 +0900

    여백 및, 글꼴 제외 디자인 완료

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet,  Image } from 'react-native';
+import { View, Text, TextInput, StyleSheet,  Image, TouchableOpacity } from 'react-native';
 import {WithLocalSvg} from 'react-native-svg/css';
 import * as Font from "expo-font";
 import Logo from './assets/img1.svg';
@@ -22,27 +22,31 @@ const SignUpScreen = () => {
 
   return (
     <View style={styles.container}>
+    
+        <Text style={styles.logo}>Spot</Text>
+        <WithLocalSvg style={styles.Logoimg} asset={Logo}/> 
+        <Text style={styles.label}>회원가입</Text>
       
-      <Text style={styles.logo}>Spot</Text>
 
-      <WithLocalSvg style={styles.Logo} asset={Logo}/> 
-      
 
-      <Text style={styles.label}>회원가입</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="닉네임"
-        value={nickname}
-        onChangeText={setNickname}
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="아이디"
-        value={userId}
-        onChangeText={setUserId}
-      />
+      <View style={styles.row}>
+        <Text style={styles.labelText}>닉네임</Text>
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="닉네임"
+            value={nickname}
+            onChangeText={setNickname}
+          />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.labelText}>아이디</Text>
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="아이디"
+            value={userId}
+            onChangeText={setUserId}
+          />
+      </View>
       <Text style={styles.hint}>아이디 중복여부 알림창</Text>
 
       <TextInput
@@ -60,29 +64,37 @@ const SignUpScreen = () => {
         value={confirmPassword}
         onChangeText={setConfirmPassword}
       />
+      <Text style={styles.hintPass}>비밀번호 일치 Label</Text>
 
+      <Text style={styles.labelText}>전화번호</Text>
       <View style={styles.row}>
         <TextInput
-          style={[styles.input, { flex: 1 }]}
+          style={[styles.inputTel, { flex: 1 }]}
           placeholder="전화번호"
           value={phoneNumber}
           onChangeText={setPhoneNumber}
         />
-        <Button title="전송" />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>전송</Text>
+          </TouchableOpacity>
       </View>
 
+      <Text style={styles.labelText}>인증번호</Text>
       <View style={styles.row}>
         <TextInput
-          style={[styles.input, { flex: 1 }]}
+          style={[styles.inputTel, { flex: 1 }]}
           placeholder="인증번호"
-          value={verificationCode}
-          onChangeText={setVerificationCode}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
         />
-        <Button title="확인"  />
-      </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>확인</Text>
+          </TouchableOpacity>
+    </View>
 
-      <Button title="다음"  />
-
+      <TouchableOpacity style={[styles.nextButton]}>
+        <Text style={styles.buttonText}>다음</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -90,44 +102,95 @@ const SignUpScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
     backgroundColor: '#fff',
+    marginLeft: 58,
   },
   logo: {
     fontSize: 36,
     fontWeight: 'bold',
     color: '#6e2fad',
     textAlign: 'center',
-    marginBottom: 30,
+    marginBottom: 5,
+    marginTop: 30,
+    marginRight: 58,
+    
   },
   label: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 18,
+    marginTop: 20,
   },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
-    padding: 10,
+    padding: 5,
     marginBottom: 10,
+    marginRight: 57,
     borderRadius: 5,
   },
   hint: {
-    fontSize: 12,
+    fontSize: 11,
     color: '#888',
-    marginBottom: 10,
+    marginBottom: 18,
+    marginTop:-2,
+  },
+  hintPass: {
+    fontSize: 11,
+    color: '#888',
+    marginBottom: 18,
+    marginTop:-2,
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 10,
   },
-  Logo: {
-    width: 50,  
+  Logoimg: {
+    width: 100,
     height: 10,  
     alignSelf: 'center',
     marginBottom: 20,
+    marginRight: 58,
   },
+  labelText: {
+    marginRight: 30,  // Text와 TextInput 간 간격 조정
+    fontSize: 16,
+    marginBottom:10,
+    
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft: 14,
+    marginRight: 58,
+    borderStyle:'solid',
+    borderWidth: 0.5,
+  },
+  inputTel: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#000000',
+    fontSize: 16,
+    textAlign: 'center',
+    borderStyle:'solid',
+  },
+  nextButton: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginRight: 58,
+    borderStyle:'solid',
+    borderWidth: 0.5,
+    marginTop: 20,
+  }
 });
 
 export default SignUpScreen;
