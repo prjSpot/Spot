84af95123601ff77041f2c1b71096d1b048d35e3
vloeiolzlr <yoonseed1995@naver.com>
2024-09-18 15:57:47 +0900

    Test

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, ScrollView, Text, TextInput, StyleSheet, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, TextInput, StyleSheet,  Image, TouchableOpacity } from 'react-native';
 import {WithLocalSvg} from 'react-native-svg/css';
 import * as Font from "expo-font";
 import Logo from './assets/img1.svg';
-import AgreeLine from "./assets/agreeline.svg";
-import CheckFalse from "./assets/checkboxfalse.svg";
-import CheckTrue from "./assets/checkboxtrue.svg";
 
 
 
@@ -14,15 +11,6 @@ import CheckTrue from "./assets/checkboxtrue.svg";
 
 const SignUpScreen = () => {
 
-  // 할 게 너무 많아요 ㅋㅋ 이게 맞나?
-  // 아이디 입력할 때마다 하단 중복여부 알림창에 사용가능 or 불가능 표기
-  // 비밀번호 두개 같은 지 확인 (일치 label)
-  // 전화번호 전송버튼 작동 -> 랜덤 숫자 6자리 생성 후 문자 혹은 카톡 전달
-  // 인증번호 생성된 랜덤번호와 일치하는지 확인
-  // 위 조건들 하나라도 만족 못하면 다음버튼 눌렀을 때 ㅁㅁ을 다시 확인해주세요
-  // 조건 다 통과되면 동의 모달 창 띄우기
-  // 모달 창 위에서 필수 동의 사항들 체크 되어있으면 다음 버튼 눌렀을 때 로그인 화면으로 이동
-
   const [nickname, setNickname] = useState('');
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
@@ -30,11 +18,6 @@ const SignUpScreen = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [verificationCode, setVerificationCode] = useState('');
   const [fontsLoaded, setFontsLoaded] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isAgreeAll, setIsAgreeAll] = useState(false);
-  const [isAgreeTerms, setIsAgreeTerms] = useState(false);
-  const [isAgreePrivacy, setIsAgreePrivacy] = useState(false);
-  const [isAgreeLocation, setIsAgreeLocation] = useState(false);
 
   const loadFonts = async () => {
     await Font.loadAsync({
@@ -60,30 +43,6 @@ const SignUpScreen = () => {
     return null;
   }
 
-  // 동의 모달 창
-  const openModal = () => {
-    setIsModalVisible(true);
-  };
-  const closeModal = () => {
-    setIsModalVisible(false);
-  };
-
-  // 모달 창 체크박스
-  const handleAgreeAll = () => {
-    const newAgreeAllState = !isAgreeAll;
-    setIsAgreeAll(newAgreeAllState);
-    setIsAgreeTerms(newAgreeAllState);
-    setIsAgreePrivacy(newAgreeAllState);
-    setIsAgreeLocation(newAgreeAllState);
-  };
-
-  const handleAgreeTerms = () => setIsAgreeTerms(!isAgreeTerms);
-  const handleAgreePrivacy = () => setIsAgreePrivacy(!isAgreePrivacy);
-  const handleAgreeLocation = () => setIsAgreeLocation(!isAgreeLocation);
-
-  const CheckBtn = (isChecked) => {
-    return isChecked ? CheckTrue : CheckFalse;
-  };
 
   return (
 
@@ -91,143 +50,76 @@ const SignUpScreen = () => {
     
         <Text style={styles.logo}>Spot</Text>
         <WithLocalSvg style={styles.Logoimg} asset={Logo}/> 
-
-      <ScrollView style={styles.signup}>
         <Text style={styles.label}>회원가입</Text>
       
 
 
-        <View style={[styles.row, { marginBottom: 10 }]}>
-          <Text style={styles.labelText}>닉네임</Text>
+      <View style={styles.row}>
+        <Text style={styles.labelText}>닉네임</Text>
           <TextInput
-            style={[styles.input, { flex: 1 , fontFamily: 'SCDream3' }]}
+            style={[styles.input, { flex: 1 }]}
             placeholder="닉네임"
             value={nickname}
             onChangeText={setNickname}
           />
-        </View>
-        <View style={styles.row}>
-          <Text style={styles.labelText}>아이디</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.labelText}>아이디</Text>
           <TextInput
-            style={[styles.input, { flex: 1 ,fontFamily: 'SCDream3'}]}
+            style={[styles.input, { flex: 1 }]}
             placeholder="아이디"
             value={userId}
             onChangeText={setUserId}
           />
-        </View>
-        <Text style={styles.hint}>아이디 중복여부 알림창</Text>
+      </View>
+      <Text style={styles.hint}>아이디 중복여부 알림창</Text>
 
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호 확인"
+        secureTextEntry
+        value={confirmPassword}
+        onChangeText={setConfirmPassword}
+      />
+      <Text style={styles.hintPass}>비밀번호 일치 Label</Text>
+
+      <Text style={styles.labelText}>전화번호</Text>
+      <View style={styles.row}>
         <TextInput
-          style={[styles.input, { marginBottom: 20 , fontFamily: 'SCDream3'}]}
-          placeholder="비밀번호"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
+          style={[styles.inputTel, { flex: 1 }]}
+          placeholder="전화번호"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
         />
-
-        <TextInput
-          style={[styles.input, { fontFamily: 'SCDream3'}]}
-          placeholder="비밀번호 확인"
-          secureTextEntry
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-        />
-        <Text style={styles.hintPass}>비밀번호 일치 Label</Text>
-
-        <Text style={styles.labelText}>전화번호</Text>
-        <View style={styles.row}>
-          <TextInput
-            style={[styles.inputTel, { flex: 1 , fontFamily: 'SCDream3'}]}
-            placeholder="전화번호"
-            value={phoneNumber}
-            onChangeText={setPhoneNumber}
-          />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>전송</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>전송</Text>
           </TouchableOpacity>
-        </View>
+      </View>
 
-        <Text style={styles.labelText}>인증번호</Text>
-        <View style={styles.row}>
-          <TextInput
-            style={[styles.inputTel, { flex: 1 , fontFamily: 'SCDream3'}]}
-            placeholder="인증번호"
-            value={verificationCode}
-            onChangeText={setVerificationCode}
-          />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>확인</Text>
-            </TouchableOpacity>
-        </View>
+      <Text style={styles.labelText}>인증번호</Text>
+      <View style={styles.row}>
+        <TextInput
+          style={[styles.inputTel, { flex: 1 }]}
+          placeholder="인증번호"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>확인</Text>
+          </TouchableOpacity>
+    </View>
 
-        <TouchableOpacity style={[styles.nextButton]}>
-          <Text style={styles.buttonText} onPress={openModal}>다음</Text>
-        </TouchableOpacity>
-      </ScrollView>
-
-      {/* Modal */}
-      <Modal
-        transparent={true}
-        visible={isModalVisible}
-        animationType="slide"
-        onRequestClose={closeModal}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <View style={[{ width: '75%'}]}>
-              <View style={[ styles.row, {justifyContent: 'space-between'} ]}>
-                <Text style={styles.modalTitle}>약관 전체동의</Text>
-                <TouchableOpacity onPress={handleAgreeAll}>
-                  <WithLocalSvg asset={CheckBtn(isAgreeAll)} style={[ styles.checkAgree, {marginBottom: 20,}]}/>
-                </TouchableOpacity>
-              </View>
-              <WithLocalSvg 
-                style={styles.agreeLine}
-                asset={AgreeLine} 
-              />
-              <ScrollView>
-                <View style={[ styles.row, {justifyContent: 'space-between'} ]}>
-                  <View style={[{flexDirection: 'row', alignItems: 'center'}]}>
-                    <Text style={styles.agreeRequired}>필수</Text>
-                    <Text style={styles.modalText}>
-                      이용약관 동의
-                    </Text>
-                  </View>
-                  <TouchableOpacity onPress={handleAgreeTerms}>
-                    <WithLocalSvg asset={CheckBtn(isAgreeTerms)} style={[ styles.checkAgree, {marginBottom: 30,}]}/>
-                  </TouchableOpacity>
-                </View>
-                <View style={[ styles.row, {justifyContent: 'space-between'} ]}>
-                  <View style={[{flexDirection: 'row'}]}>
-                    <Text style={styles.agreeRequired}>필수</Text>
-                    <Text style={styles.modalText}>
-                      개인정보 수집 및 이용 동의
-                    </Text>
-                  </View>
-                  <TouchableOpacity onPress={handleAgreePrivacy}>
-                    <WithLocalSvg asset={CheckBtn(isAgreePrivacy)} style={[ styles.checkAgree, {marginBottom: 30,}]}/>
-                  </TouchableOpacity>
-                </View>
-                <View style={[ styles.row, {justifyContent: 'space-between'} ]}>
-                  <View style={[{flexDirection: 'row'}]}>
-                    <Text style={[styles.agreeRequired, {color: 'black'}]}>선택</Text>
-                    <Text style={styles.modalText}>
-                      위치 정보 서비스 이용약관 동의
-                    </Text>
-                  </View>
-                  <TouchableOpacity onPress={handleAgreeLocation}>
-                    <WithLocalSvg asset={CheckBtn(isAgreeLocation)} style={[ styles.checkAgree, {marginBottom: 30,}]}/>
-                  </TouchableOpacity>
-                </View>
-                <TouchableOpacity style={[styles.nextButton, {marginTop:10}]} onPress={closeModal}>
-                  <Text style={styles.buttonText}>다음</Text>
-                </TouchableOpacity>
-              </ScrollView>
-            </View>
-          </View>
-        </View>
-      </Modal>
-
+      <TouchableOpacity style={[styles.nextButton]}>
+        <Text style={styles.buttonText}>다음</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -236,21 +128,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: "center",
-  },
-  signup: {
-    width: "75%"
+    marginLeft: 58,
   },
   logo: {
     fontSize: 36,
     fontFamily:'Lobster',
-    color: '#8236E2',
-    marginBottom: 0,
-    marginTop: "16%",
+    color: '#6e2fad',
+    textAlign: 'center',
+    marginBottom: 5,
+    marginTop: 30,
+    marginRight: 58,
+    
   },
   label: {
     fontFamily: 'SCDream5',
     fontSize: 20,
+    
     marginBottom: 18,
     marginTop: 20,
   },
@@ -259,57 +152,62 @@ const styles = StyleSheet.create({
     borderColor: '#ccc',
     padding: 5,
     marginBottom: 10,
+    marginRight: 57,
     borderRadius: 5,
-    backgroundColor: '#E6E9F3',
-    color: 'black',
+    fontFamily: 'SCDream1',
   },
   hint: {
     fontSize: 11,
     color: '#888',
     marginBottom: 18,
+    marginTop:-2,
     fontFamily:'SCDream2'
   },
   hintPass: {
     fontSize: 11,
     color: '#888',
     marginBottom: 18,
+    marginTop:-2,
     fontFamily:'SCDream2'
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 10,
   },
   Logoimg: {
     width: 100,
     height: 10,  
     alignSelf: 'center',
     marginBottom: 20,
+    marginRight: 58,
   },
   labelText: {
     fontFamily:'SCDream3',
+    marginRight: 30,  
     fontSize: 16,
     marginBottom:10,
-    marginRight: '7%',
+    
   },
   button: {
     backgroundColor: '#ffffff',
     paddingVertical: 10,
-    paddingHorizontal: 15,
+    paddingHorizontal: 20,
     borderRadius: 5,
+    marginLeft: 14,
+    marginRight: 58,
     borderStyle:'solid',
     borderWidth: 0.5,
-    marginBottom: 20,
   },
   inputTel: {
     borderWidth: 1,
     borderColor: '#ccc',
-    backgroundColor: '#E6E9F3',
     padding: 5,
     borderRadius: 5,
-    marginBottom: 20,
-    marginRight: '7%',
+    fontFamily: 'SCDream1',
   },
   buttonText: {
+    color: '#000000',
     fontSize: 16,
     textAlign: 'center',
     borderStyle:'solid',
@@ -320,47 +218,10 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
+    marginRight: 58,
     borderStyle:'solid',
     borderWidth: 0.5,
     marginTop: 20,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    paddingTop: 30,
-    height: '50%',
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontFamily: 'SCDream6',
-    color: '#8236E2',
-    marginBottom: 20,
-  },
-  agreeRequired: {
-    color:'#8236E2', 
-    fontFamily: 'SCDream5', 
-    marginRight: 10, 
-    marginBottom:30,
-  },
-  modalText: {
-    fontSize: 16,
-    fontFamily: 'SCDream3',
-    marginBottom: 30,
-  },
-  agreeLine: {
-    alignSelf: 'center',
-    marginBottom: 30,
-    width: 300,
-  },
-  checkAgree: {
-    marginLeft: 10,
   }
 });
 
